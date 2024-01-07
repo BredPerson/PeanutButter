18059aa33581a13d33903badb5d70e78092c3767
BredPerson <155865812+BredPerson@users.noreply.github.com>
2024-01-07 04:55:14 +0000

    I do Some stuff today

---
+++ JELLY.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.DelayQueue;
+
+public class JELLY {   
+    
+    public static void main(String[] args)
+        {
+            String name = "Pasta";
+            System.out.println(name);
+
+            int myNum = 718;
+            System.out.println(myNum);
+
+            if(myNum == 718){
+               System.out.println("Don't get a girlfriend"); 
+            }
+            else{
+                System.out.println("get a girlfriend");
+            }
+            
+        }   
+}
